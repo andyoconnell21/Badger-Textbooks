95470aed7223fe8b3feb91612ce3a8f97452c46c
andyoconnell21 <51180550+andyoconnell21@users.noreply.github.com>
2021-03-22 14:32:11 -0500

    Update App.js
    Made own Login page, app.js now for navigation

--- badger-textbooks-app/src/App.js
+++ badger-textbooks-app/src/App.js
@@ -13,6 +13,8 @@ import {
   } from "@material-ui/core";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CreateAccount from "./pages/CreateAccount"
+import Home from "./pages/Home"
+import Login from "./Login"
 import React from "react";
 
 const firebaseConfig = {
@@ -30,92 +32,17 @@ var fire = firebase.initializeApp(firebaseConfig);
 
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    }
-  }
-
-  setEmail = (event) => {
-    this.setState({email: event.target.value})
-  }
-
-  setPassword = (event) => {
-    this.setState({password: event.target.state})
-  }
-
-  authWithAccountCreds = (event) => {
-
-  }
-
-  forgotPassword = (event) => {
-    alert("Password link sent")
-  }
-
-  createAccount = (event) => {
-    event.preventDefault();
-    window.location.href = '/createaccount'
-  }
 
   render() {
     return (
-      <div>
+      <div className="App">
         <Router>
           <Switch>
+            <Route path="/" exact component={() => <Login />}/>
             <Route path="/createaccount" exact component={() => <CreateAccount />}/>
+            <Route path="/home" exact component={() => <Home />} />
           </Switch>
         </Router>
-        <AppBar position = "static" style={{background:'#c5050c'}}>
-          <Typography variant='h6' style={{flexGrow: 1, fontFamily: 'sans-serif', fontSize: '25px', margin: '25px', textAlign: 'center'}}>
-            Welcome to Badger Textbooks
-          </Typography>
-        </AppBar>
-
-        <Container component="main" maxWidth="xs" style={{marginTop: '40px', fontFamily: 'sans-serif'}}>
-          <Typography component="h1" variant="h4" style={{textAlign: 'center'}}>
-            Sign in
-          </Typography>
-          <form onSubmit={this.authWithAccountCreds}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              type="email"
-              onChange = {this.setEmail}
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              onChange = {this.setPassword}
-            />
-            <Button type="submit" style={{marginTop: "10px", marginBottom: '10px', border: '0', backgroundColor: '#c5050c', width: '50%', marginRight: '25%', marginLeft: '25%', cursor: 'pointer', color: 'white', fontSize: '18px'}}>Log In</Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" onClick={this.forgotPassword} variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" onClick={this.createAccount} variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </Container>
       </div>
     );
   }
