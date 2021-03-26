6a77850f14267b57f451733a5c4f9974d4edc9f4
andyoconnell21 <51180550+andyoconnell21@users.noreply.github.com>
2021-03-26 18:53:26 -0500

    Update App.test.js
    Added render tests for Login Page and Create Account Page

--- badger-textbooks-app/src/App.test.js
+++ badger-textbooks-app/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import Home from './pages/Home';
+import Login from './Login'
+import CreateAccount from './pages/CreateAccount';
 
 describe("Testing Home-Page", () => {
   test('basic render test', () => {
@@ -13,6 +15,32 @@ describe("Testing Home-Page", () => {
     expect(/Search Listings By.../i).toBeInTheDocument();
   })
 
+describe("Render Testing of Login Page", () =>{
+  test("Basic Render Test of Login Page", () => {
+    render(<Login />);
+  })
+  test("Render LoginPage Text", () => {
+      render(<Login />);
+      const headerText = screen.getByText("Welcome to Badger Textbooks")
+      const signInText = screen.getByText("Sign In")
+      expect(headerText).toBeInTheDocument();
+      expect(signInText).toBeInTheDocument();
+    })
+})
+
+describe("Render Testing of Create Account Page", () =>{
+  test("Basic Render Test of Create Account Page", () => {
+    render(<CreateAccount />)
+  })
+
+  test("Render CreateAccount Page Text", () => {
+    render(<CreateAccount />)
+    const headerText = screen.getByText("Welcome to Badger Textbooks")
+    const createAccountText = screen.getByText("Create An Account")
+    expect(headerText).toBeInTheDocument();
+    expect(createAccountText).toBeInTheDocument();
+  })
+})
   
   
 });
