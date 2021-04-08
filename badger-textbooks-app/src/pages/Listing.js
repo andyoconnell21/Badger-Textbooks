c08937b3f6f6f55e79735b491de4551002617ac7
andyoconnell21 <51180550+andyoconnell21@users.noreply.github.com>
2021-04-08 09:19:11 -0500

    Update Listing.js
    Updated Listing Page UI

--- badger-textbooks-app/src/pages/Listing.js
+++ badger-textbooks-app/src/pages/Listing.js
@@ -14,6 +14,12 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
 
 import HomeIcon from '@material-ui/icons/Home';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -21,10 +27,7 @@ import AddIcon from '@material-ui/icons/Add';
 import MyListingsIcon from '@material-ui/icons/ListAlt';
 import AccountIcon from '@material-ui/icons/AccountCircle';
 import SavedIcon from '@material-ui/icons/Favorite';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-
+import ChatIcon from '@material-ui/icons/Chat';
 
 class Listings extends React.Component {
     constructor(props){
@@ -44,7 +47,6 @@ class Listings extends React.Component {
     }
 
     componentDidMount () {
-        
       firebase.auth().onAuthStateChanged(function(user) {
         if (!user) {
           //User is not siged in...redirect to login page
@@ -110,6 +112,11 @@ class Listings extends React.Component {
                 <ListItemText primary="My Listings" />
               </ListItem>
               <Divider/>
+              <ListItem button key="chat_list_nav" onClick={() => {window.location.href = "/chatlistpage";}}>
+                <ListItemIcon><ChatIcon/></ListItemIcon>
+                <ListItemText primary="My Conversations" />
+              </ListItem>
+              <Divider/>
               <ListItem button key="saved_listings_nav" disabled>
                 <ListItemIcon><SavedIcon/></ListItemIcon>
                 <ListItemText primary="Saved Listings" />
@@ -122,52 +129,72 @@ class Listings extends React.Component {
           </List>
         </Drawer>
 
-        <h2>Title: {this.state.title}</h2>
-        <h3>Author: {this.state.author}</h3>
-        <h3>Class: {this.state.class}</h3>
-        <h3>Condition: {this.state.condition}</h3>
-        <h3>Price: ${this.state.price}</h3>
-        <img src={this.state.image} alt="textbook" style={{marginBottom: "15px", width:'20%'}}/>
-        <Accordion style={{width: "25%", margin: 'auto'}}>
-          <AccordionSummary style={{backgroundColor: 'lightgrey'}}>
-            <Typography style={{fontFamily: 'sans-serif', fontSize:'14px', fontWeight: 'bold', margin:'auto'}}>MORE INFORMATION</Typography>
-          </AccordionSummary>
-          <AccordionDetails style={{flexDirection: 'column'}}>
-            <Typography>
-              <b>Seller of Textbook:</b> {this.state.owner}
-            </Typography>
-            <Typography>
-              <b>ISBN: </b> {this.state.ISBN}
-            </Typography>
-            <Typography>
-              <b>Date of Listing:</b> {this.state.date.toString()}
-            </Typography>
-            <Typography>
-              <b>Rating of Seller:</b> 5 Star
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        
-        <Button 
-          fullWidth 
-          style={{
-            marginTop: "10px", 
-            marginBottom: '10px', 
-            border: '0', 
-            backgroundColor: '#c5050c', 
-            width: '25%', 
-            marginRight: '25%', 
-            marginLeft: '25%', 
-            cursor: 'pointer', 
-            color: 'white', 
-            fontSize: '18px'}} 
-            onClick={() => {
-              sessionStorage.setItem("receiverEmail", this.state.owner);
-              sessionStorage.setItem("returnLocation", "/listing");
-              window.location.href = "/testChatPage";
-            }}>
-            Chat with Seller! 
-          </Button>
+        <Grid container>
+          <Card style={{width: "600px", margin:'auto', marginTop: '50px'}}>
+            <CardContent>
+              <Grid container>
+                <Grid item>
+                  <img src={this.state.image} width="250" height="250" alt="Textbook Cover"/>
+                </Grid>
+                <Grid item xs >
+                  <Typography variant='h6'>
+                    <b>Title:</b> {this.state.title}
+                  </Typography>
+                  <Typography variant='h6'>
+                    <b>Author:</b> {this.state.author}
+                  </Typography>
+                  <Typography variant='h6'>
+                    <b>Class:</b> {this.state.class}
+                  </Typography>
+                  <Typography variant='h6'>
+                    <b>Condition:</b> {this.state.condition}
+                  </Typography>
+                  <Typography variant='h6'>
+                    <b>Price:</b> {this.state.price}$
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Divider style={{marginTop: "10px", marginBottom: "10px"}}/>
+              <Accordion style={{width: "75%", margin: 'auto'}}>
+                <AccordionSummary style={{backgroundColor: 'lightgrey'}}>
+                  <Typography style={{fontFamily: 'sans-serif', fontSize:'14px', fontWeight: 'bold', margin:'auto'}}>MORE INFORMATION</Typography>
+                </AccordionSummary>
+                <AccordionDetails style={{flexDirection: 'column'}}>
+                  <Typography>
+                    <b>Seller of Textbook:</b> {this.state.owner}
+                  </Typography>
+                  <Typography>
+                    <b>ISBN: </b> {this.state.ISBN}
+                  </Typography>
+                  <Typography>
+                    <b>Date of Listing:</b> {this.state.date.toString()}
+                  </Typography>
+                  <Typography>
+                    <b>Rating of Seller:</b> 5 Star
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Button 
+                fullWidth 
+                style={{
+                  marginTop: "10px", 
+                  marginBottom: '10px', 
+                  border: '0', 
+                  backgroundColor: '#c5050c', 
+                  width: '75%', 
+                  cursor: 'pointer', 
+                  color: 'white', 
+                  fontSize: '18px'}} 
+                  onClick={() => {
+                    sessionStorage.setItem("receiverEmail", this.state.owner);
+                    sessionStorage.setItem("returnLocation", "/listing");
+                    window.location.href = "/testChatPage";
+                  }}>
+                  Chat with Seller! 
+                </Button>
+            </CardContent>
+          </Card>
+        </Grid>
       </div>
      )
   }
