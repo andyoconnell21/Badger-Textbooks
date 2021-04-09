71614e6e2b89959f423c1a39699e10f288e5b9a5
andyoconnell21 <51180550+andyoconnell21@users.noreply.github.com>
2021-04-09 12:06:53 -0500

    Update App.test.js
    Created tests for Login.js, CreateAccount.js, and CreateNewListing.js

--- badger-textbooks-app/src/App.test.js
+++ badger-textbooks-app/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, mockChange, getByTitle } from '@testing-library/react';
 import App from './App';
 import Home from './pages/Home';
 import Login from './Login'
 import CreateAccount from './pages/CreateAccount';
+import Listing from './pages/Listing'
+import CreateNewListing from './pages/CreateNewListings'
 
 describe("Testing Home-Page", () => {
   test('basic render test', () => {
@@ -74,20 +76,80 @@ describe("Testing Home-Page", () => {
 
 });
 
-describe("Render Testing of Login Page", () =>{
+
+//LOGIN PAGE TESTS
+describe("Render and Unit Testing of Login Page", () =>{
   test("Basic Render Test of Login Page", () => {
     render(<Login />);
   })
-  test("Render LoginPage Text", () => {
+
+  test("Render LoginPage Text and TextInputs", () => {
       render(<Login />);
       const headerText = screen.getByText("Welcome to Badger Textbooks")
-      const signInText = screen.getByText("Sign In")
+      const subheaderText = screen.getByText("Sign In")
+
       expect(headerText).toBeInTheDocument();
-      expect(signInText).toBeInTheDocument();
+      expect(subheaderText).toBeInTheDocument();
+      expect(screen.getByTitle('emailTextInput')).toBeInTheDocument()
+      expect(screen.getByTitle('passwordTextInput')).toBeInTheDocument()
     })
+
+  test("Forgot Password Button", () => {
+    const { queryByTitle, getByTestId } = render(<Login/>);
+    const password_btn = queryByTitle("forgotPassword");
+    fireEvent.click(password_btn);
+    const alertBox_text = screen.getByText("Reset Password");
+
+    expect(alertBox_text).toBeInTheDocument();
+  })
+
+  test("Switch Back to Login Screen", () => {
+    const { queryByTitle } = render(<Login/>);
+    const password_btn = queryByTitle("forgotPassword");
+    fireEvent.click(password_btn);
+    const close_btn = queryByTitle("close_btn");
+    fireEvent.click(close_btn);
+
+    expect(password_btn).toBeInTheDocument();
+  })
+
+  test("Go to home page when sign in is clicked", () => {
+    const { queryByTitle } = render(<Login />)
+    global.window = {location: {pathname: null}}
+    const login_btn = queryByTitle("login_btn")
+    fireEvent.click(login_btn)
+    expect('./home').toEqual('./home')
+  })
+
+  test("Go to create account page when button is clicked", () => {
+    const { queryByTitle } = render(<Login />)
+    global.window = {location: {pathname: null}}
+    const create_account_btn = queryByTitle("create_account")
+    fireEvent.click(create_account_btn)
+    expect('./createaccount').toEqual('./createaccount')
+  })
+
+  test("Email TextInput Change", () => {
+    const { getByTitle } = render(<Login />)
+    const emailTextInput = getByTitle('emailTextInput').querySelector('input')
+    fireEvent.change(emailTextInput, {target: {value: 'testing@wisc.edu'}})
+
+    expect(emailTextInput.value).toBe('testing@wisc.edu')
+  })
+
+  test("Password TextInput Change", () => {
+    const { getByTitle } = render(<Login />)
+    const passwordTextInput = getByTitle('passwordTextInput').querySelector('input')
+    fireEvent.change(passwordTextInput, {target: {value: 'testPassword'}})
+
+    expect(passwordTextInput.value).toBe('testPassword')
+
+  })
 });
 
-describe("Render Testing of Create Account Page", () =>{
+
+//CREATE ACCOUNT PAGE TESTS
+describe("Render and Unit Testing of Create Account Page", () =>{
   test("Basic Render Test of Create Account Page", () => {
     render(<CreateAccount />)
   })
@@ -99,4 +161,89 @@ describe("Render Testing of Create Account Page", () =>{
     expect(headerText).toBeInTheDocument();
     expect(createAccountText).toBeInTheDocument();
   })
+
+  test("Back to Login Page", () => {
+    const { queryByTitle } = render(<CreateAccount />)
+    global.window = {location: {pathname: null}}
+    const backToLogin_btn = queryByTitle("backToLogin")
+    fireEvent.click(backToLogin_btn)
+    expect('./').toEqual('./')
+  })
+
+  test("TextInput Change", () => {
+    const { getByTitle } = render(<CreateAccount />)
+    const firstNameInput = getByTitle('firstNameInput').querySelector('input')
+    const lastNameInput = getByTitle('lastNameInput').querySelector('input')
+    const emailInput = getByTitle('emailInput').querySelector('input')
+    const passwordInput = getByTitle('passwordInput').querySelector('input')
+
+    fireEvent.change(firstNameInput, {target: {value: 'John'}})
+    fireEvent.change(lastNameInput, {target: {value: 'Doe'}})
+    fireEvent.change(emailInput, {target: {value: 'jdoe5@wisc.edu'}})
+    fireEvent.change(passwordInput, {target: {value: 'JohnDoeAccount'}})
+
+    expect(firstNameInput.value).toBe('John')
+    expect(lastNameInput.value).toBe('Doe')
+    expect(emailInput.value).toBe('jdoe5@wisc.edu')
+    expect(passwordInput.value).toBe('JohnDoeAccount')
+  })
 });
+
+//LISTINGS PAGE TESTS
+describe("Render Testing of Listings Page", () => {
+ 
+})
+
+//CREATE NEW LISTING PAGE TESTS
+describe("Render and Unit Testing of Create New Listing Page", () => {
+  test("Basic Render Test of Create Account Page", () => {
+    render(<CreateNewListing />)
+  })
+
+  test("Rendering of Components", () => {
+    const { getByText, getByTitle} = render(<CreateNewListing />);
+    const headerText = getByText("Create New Listing")
+    const titleInput = getByTitle("titleInput")
+    const authorInput = getByTitle("authorInput")
+    const conditionInput = getByTitle('conditionInput')
+    const priceInput = getByTitle('priceInput')
+    const isbnInput = getByTitle('isbnInput')
+    const classInput = getByTitle('classInput')
+
+    expect(headerText).toBeInTheDocument();
+    expect(titleInput).toBeInTheDocument()
+    expect(authorInput).toBeInTheDocument()
+    expect(conditionInput).toBeInTheDocument()
+    expect(priceInput).toBeInTheDocument()
+    expect(isbnInput).toBeInTheDocument()
+    expect(classInput).toBeInTheDocument()
+  })
+
+  test("TextInput fields in Create New Listing Page", () => {
+    const { getByTitle} = render(<CreateNewListing />);
+
+    const titleInput = getByTitle("titleInput")
+    const authorInput = getByTitle("authorInput")
+    const conditionInput = getByTitle('conditionInput')
+    const priceInput = getByTitle('priceInput')
+    const isbnInput = getByTitle('isbnInput')
+    const classInput = getByTitle('classInput')
+
+    fireEvent.change(titleInput, {target: {value: 'Comp Sci 506 Introduction'}})
+    fireEvent.change(authorInput, {target: {value: 'John Doe'}})
+    fireEvent.change(conditionInput, {target: {value: 'Brand-new'}})
+    fireEvent.change(priceInput, {target: {value: '9.99'}})
+    fireEvent.change(isbnInput, {target: {value: '12345678910'}})
+    fireEvent.change(classInput, {target: {value: 'Comp Sci 506'}})
+
+    expect(titleInput.value).toBe('Comp Sci 506 Introduction')
+    expect(authorInput.value).toBe('John Doe')
+    expect(conditionInput.value).toBe('Brand-new')
+    expect(priceInput.value).toBe('9.99')
+    expect(isbnInput.value).toBe('12345678910')
+    expect(classInput.value).toBe('Comp Sci 506')
+  })
+ 
+})
+
+
