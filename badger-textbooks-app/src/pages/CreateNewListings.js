035db4cec608eb7641f77ca2dd686b2fb6607d01
NicolasM42 <mewhirter@wisc.edu>
2021-04-25 20:43:07 -0500

    Updated tab logo, implemented image loading bar.
    Logo on tab now has red background.
    When uploading an image, a progress bar now displays (sometimes the image still doesn't show up though :/)

--- badger-textbooks-app/src/pages/CreateNewListings.js
+++ badger-textbooks-app/src/pages/CreateNewListings.js
@@ -47,6 +47,7 @@ export class CreateNewListing extends Component {
             alertOpen: false,
             menuOpen: false,
             imageURL: '',
+            imageProgress: 0,
             sellerName: '',
         }
     }
@@ -161,12 +162,21 @@ export class CreateNewListing extends Component {
         var storage = firebase.storage()
         var storageRef = storage.ref()
         const fileRef = storageRef.child(file.name)
-        fileRef.put(file).then(() => {
-            //Saved image file path
-            storageRef.child(file.name).getDownloadURL().then((url) => {
-                this.setState({imageURL: url})
-            })
-        })
+        fileRef.put(file).on('state_changed', (snapshot) => {
+            //console.log(snapshot.bytesTransferred);
+            this.setState({imageProgress: (snapshot.bytesTransferred / snapshot.totalBytes) * 100});
+            if (snapshot.bytesTransferred === snapshot.totalBytes) {
+                storageRef.child(file.name).getDownloadURL().then((url) => {
+                    this.setState({imageURL: url});
+                })
+            }
+        });
+        // }).then(() => {
+        //     //Saved image file path
+        //     storageRef.child(file.name).getDownloadURL().then((url) => {
+        //         this.setState({imageURL: url})
+        //     })
+        // })
     }
 
     render() {
@@ -283,6 +293,7 @@ export class CreateNewListing extends Component {
                                 type='file'
                                 onChange={this.handleImageChange}
                             />
+                            <progress id="progressBar" value={this.state.imageProgress} max="100"></progress>
                         </div>
                         <Divider/>
                         <Button 
